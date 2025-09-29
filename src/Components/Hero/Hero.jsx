81c2c8c00068edc29f8dccc735628140a859f6a5
Gary Wen <garywn123@gmail.com>
2025-09-29 13:04:51 -0500

    Initial commit - portfolio website

---
+++ src/Components/Hero/Hero.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import './Hero.css'
+import profile_pic from '../../Assets/profile-pic.png'
+import AnchorLink from 'react-anchor-link-smooth-scroll'
+
+const Hero = () => {
+  return (
+    <div id='home' className='hero'>
+      <img src={profile_pic} alt="Profile" />
+      <h1><span>Hi! I'm Gary Wen,</span> Developer based in Dallas Texas.</h1>
+      <p>I am a full-stack developer that specializes in building exceptional digital experiences.</p>
+      <div className="hero-action">
+        <div className="hero-connect"><AnchorLink className='anchor-link' href='#contact'>Connect With Me</AnchorLink></div>
+        <div className="hero-resume">My Resume</div>
+      </div>
+    </div>
+  )
+}
+
+export default Hero
